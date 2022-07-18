6dd8926ee47b309062952924f0c179e51974f2de
Hanno Braun <hanno@braun-odw.eu>
2022-07-18 18:36:49 +0200

    Document purpose of Fornjot model

--- calculation/src/lib.rs
+++ calculation/src/lib.rs
@@ -1,3 +1,28 @@
+//! Calculations for the CNC mill
+//!
+//! This is basically a Rust program, in the form of a [Fornjot] model, that
+//! does some calculations about the CNC mill, to help to select right-sized
+//! components.
+//!
+//! So, why is this a Fornjot model? Honestly, is doesn't make a whole lot of
+//! sense, if viewed in isolation. Fornjot doesn't have the features yet to
+//! model the CNC mill's geometry, and it has no simulation features at all. It
+//! doesn't really support anything I'm using it for here, and I'd probably be
+//! better off just doing it in Excel.
+//!
+//! However, I want to use Fornjot for modeling the CNC mill (or its successor)
+//! in the future, and it makes sense to do this kind of calculation together
+//! with the CAD model. I also want Fornjot to at least not stand in the way of
+//! use cases outside of its (current) core feature that, and using it for
+//! something it wasn't designed for should help there.
+//!
+//! Lastly, I usually only work on Fornjot itself, within its repository. Using
+//! it for something new outside of the repository was already hugely
+//! informative, in regards to some weaknesses it has, and where it might trip
+//! new users up.
+//!
+//! [Fornjot]: https://www.fornjot.app/
+
 #[fj::model]
 fn cnc() {
     // This is a placeholder. We don't actually need to export geometry right
